531ab0fa8bf65d15ab82bca8c9db2d0533efb536
Trey Smith <trey.smith@nasa.gov>
2012-01-16 10:56:28 -0800

    created geocamMapSet from template

---
+++ geocamMapSet/tests.py
@@ -0,0 +1,15 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.test import TestCase
+
+
+class geocamMapSetTest(TestCase):
+    """
+    Tests for geocamMapSet
+    """
+    def test_geocamMapSet(self):
+        pass
